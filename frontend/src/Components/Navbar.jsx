04a4e34968228e25f45bf7b528bede110b99bc4c
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-06-25 17:54:09 +0500

    Commit

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -4,26 +4,26 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <Fragment>
-                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <Link class="navbar-brand" to="#">PSL</Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <Link class="navbar-brand" to="#">PSL</Link>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <Link class="nav-link" to="/user/login">Login</Link>
-            </li>
-            <li class="nav-item active" >
-                <Link class="nav-link" to="/user/signup">Sign up</Link>
-            </li>
+                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <Link class="nav-link" to="/user/login">Login</Link>
+                        </li>
+                         <li class="nav-item active" >
+                         <Link class="nav-link" to="/user/signup">Sign up</Link>
+                        </li>
             
           
-            </ul>
+                    </ul>
            
-        </div>
-        </nav>
+                </div>
+            </nav>
            
         </Fragment>
      );
